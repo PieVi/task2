86f495349ad1e60849073213ad428f9f817caee7
Panos <panvournas@gmail.com>
2021-03-12 15:22:07 +0200

    created generator method for fizzbuzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -3,8 +3,8 @@
 import argparse
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Accepts an integer')
-    parser.add_argument('num', type=int)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('num', type=int, help='the number the fizzbuzz range will be')
     return parser.parse_args()
 
 args = parse_arguments()
@@ -20,5 +20,32 @@ def fizz_buzz(num):
         else:
             print(i+1)
 
-fizz_buzz(args.num)
+def fizz_buzz_generator(num):
+    for i in range(num):
+        if (i+1)%3==0 and (i+1)%5==0:
+            yield print('fizzbuzz')
+        elif (i+1)%3==0:
+            yield print('fizz')
+        elif (i+1)%5==0:
+            yield print('buzz')
+        else:
+            yield print(i+1)
 
+
+gen = fizz_buzz_generator(args.num)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
+next(gen)
